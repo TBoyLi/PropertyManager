9eb0c5672f86dbf6899a163a1875a6ff1dcd245a
RedLi <277099524@qq.com>
2016-09-12 17:31:37 +0800

    configure android pushy

--- android/app/src/main/java/com/propertymanager/MainApplication.java
+++ android/app/src/main/java/com/propertymanager/MainApplication.java
@@ -1,26 +1,31 @@
 package com.propertymanager;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.airbnb.android.react.maps.MapsPackage;
 import com.chinaztt.encapsulation.EncryptionReactPackager;
 import com.eguma.barcodescanner.BarcodeScannerPackage;
 import com.facebook.react.ReactApplication;
-import cn.reactnative.modules.update.UpdatePackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.imagepicker.ImagePickerPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
+import cn.reactnative.modules.update.UpdateContext;
+import cn.reactnative.modules.update.UpdatePackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected String getJSBundleFile() {
+      return UpdateContext.getBundleUrl(MainApplication.this);
+    }
+
     @Override
     protected boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -30,7 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new UpdatePackage(),
+          new UpdatePackage(),
           new PickerPackage(),
           new BarcodeScannerPackage(),
           new ImagePickerPackage(),
