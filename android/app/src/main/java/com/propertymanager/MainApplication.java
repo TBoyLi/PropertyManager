ade0475917708b22c4109f470c69967d25b7f484
RedLi <277099524@qq.com>
2016-09-20 16:09:46 +0800

    delete maps-google

--- android/app/src/main/java/com/propertymanager/MainApplication.java
+++ android/app/src/main/java/com/propertymanager/MainApplication.java
@@ -6,6 +6,7 @@ import com.airbnb.android.react.maps.MapsPackage;
 import com.chinaztt.encapsulation.EncryptionReactPackager;
 import com.eguma.barcodescanner.BarcodeScannerPackage;
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
           new UpdatePackage(),
           new PickerPackage(),
           new BarcodeScannerPackage(),
