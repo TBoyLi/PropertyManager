44076fb43e5294fb4af18570e3ece601d1f840cc
redli <277099524@qq.com>
2016-09-26 14:20:20 +0800

    map

--- app/containers/where/villageMap.js
+++ app/containers/where/villageMap.js
@@ -23,15 +23,14 @@ import KeyUrl from '../../constants/key.js';
 import HeadView from '../../components/HeadView.js';
 import Building from './building.js';
 
-var position;
-var makers;
-
 class VillageMap extends React.Component {
     // 构造
     constructor(props) {
         super(props);
         this.state =  {
-          initialPosition: 'unknown',
+          url:'http://vczero.github.io/webview/index.html?',
+          position:'unknown',
+          marks:'unknown',
         };
     }
     onLeftPress(){
@@ -44,18 +43,44 @@ class VillageMap extends React.Component {
         needDetail: true,
       });
       this.listener = AMapLocation.addEventListener((data) => {
-        console.log('data', data);
         if (data) {//获取定位
-          position = data;
-          console.log('position',position);
+          var position = data.longitude + ',' + data.latitude;
+          var url = KeyUrl.searchURL + 'key=' + KeyUrl.WEB_KEY + '&location=' + position +'&keywords=' + '住宅区' + '&extensions=base';
           AMapLocation.stopLocation();
           this.listener.remove();
+          this.doGetData(position,url);
         }else {
           ToastShort('定位失败','bottom');
         }
       });
     }
+    doGetData(position,url){
+      var marks = '';
+      this.setState({
+        position:position
+      });
+      return fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log('Json',responseJson);
+        for (var i = 0; i < responseJson.pois.length; i++) {
+          if (i <= 9) {
+            marks +=(responseJson.pois[i].location + ',')
+          }
+        }
+        this.setState({
+          marks:marks
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
     render() {
+      var allUrl;
+      if (this.state.position) {
+        allUrl = this.state.url + 'pos=' + this.state.position + '&markers=' + this.state.marks;
+      }
       return (
         <View style={styles.container}>
           <HeadView title='小区'
@@ -66,6 +91,7 @@ class VillageMap extends React.Component {
             leftButton={true}
             rightButton={false}
             navigator={this.props.navigator} />
+          <WebView source={{uri: allUrl}}/>
         </View>
       );
     }
